93d150633866d12b236abcf1115aeb36fb698fc5
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-01-17 21:26:29 -0300

    server firts routes and database prisma models configurations

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,15 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
+
 
 app.register(cors, {
   origin: 'http://localhost:3000',
 })
 
-app.get('/', async () => {
-  const habits = await prisma.habit.findMany()
-
-  return habits
-})
+app.register(appRoutes)
 
 app
   .listen({
